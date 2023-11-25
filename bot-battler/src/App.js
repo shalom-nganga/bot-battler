5386dd78301d645fcb72629ec3e46ba048fe2879
shalom-nganga <minnomacharia@gmail.com>
2023-11-25 14:47:59 +0300

    changed files

--- bot-battler/src/App.js
+++ bot-battler/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Bot from './Components/Bot';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [data, setData]=useState([])
+  useEffect(()=>{
+    fetch(`http://localhost:8000/bots`)
+    .then((res)=>res.json())
+    .then((bots)=>{
+      setData(bots)
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Routes>
+          <Route exact path='/' element={data && <Bot bots={data}/>}/>
+          
+        </Routes>
+    
     </div>
   );
 }
